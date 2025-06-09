4021f6cda5c445f4a4223624f5218846042d0a42
miguel cassiano lopes <miguellopescassiano@gmail.com>
2025-06-09 01:37:32 -0300

    Criação layout tela login

---
+++ P2-Exercicio/P2-Exercicio/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace P2_Exercicio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            lbUsuario = new Label();
+            lbSenha = new Label();
+            btLogin = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(97, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(145, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(97, 109);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(145, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // lbUsuario
+            // 
+            lbUsuario.AutoSize = true;
+            lbUsuario.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lbUsuario.Location = new Point(39, 83);
+            lbUsuario.Name = "lbUsuario";
+            lbUsuario.Size = new Size(52, 15);
+            lbUsuario.TabIndex = 2;
+            lbUsuario.Text = "Usuário:";
+            // 
+            // lbSenha
+            // 
+            lbSenha.AutoSize = true;
+            lbSenha.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lbSenha.Location = new Point(47, 112);
+            lbSenha.Name = "lbSenha";
+            lbSenha.Size = new Size(44, 15);
+            lbSenha.TabIndex = 3;
+            lbSenha.Text = "Senha:";
+            // 
+            // btLogin
+            // 
+            btLogin.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            btLogin.Location = new Point(127, 138);
+            btLogin.Name = "btLogin";
+            btLogin.Size = new Size(75, 23);
+            btLogin.TabIndex = 4;
+            btLogin.Text = "Login";
+            btLogin.UseVisualStyleBackColor = true;
+            btLogin.Click += btLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(318, 276);
+            Controls.Add(btLogin);
+            Controls.Add(lbSenha);
+            Controls.Add(lbUsuario);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label lbUsuario;
+        private Label lbSenha;
+        private Button btLogin;
+    }
+}
